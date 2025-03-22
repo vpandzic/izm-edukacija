9694d080c1f4ccaf3123067ce990ca7070b1163c
Vanja Pandzic <vpandzic@live.com>
2025-03-22 21:13:09 +0100

    add dynamic dentists

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./About.css";
+import Img from "../../utility/Img"
 
 const About = () => {
+
+    const [dentists, setDentists] = useState([]);
+    const REACT_APP_URL = process.env.REACT_APP_URL;
+
+    useEffect(() => {
+        fetch(REACT_APP_URL + 'wp-json/wp/v2/posts?categories=230')
+            .then(response => response.json())
+            .then(data => setDentists(data))
+    }, [REACT_APP_URL]);
+
     return (
         <div className="about-container">
             <section id="about">
                 <div className="container">
+
                     <h1>O našem timu</h1>
                     <p>Upoznajte vrhunski tim stomatologa koji se brine o vašem osmijehu.</p>
 
                     <div className="row">
-                        <div className="col-md-4 team-member">
-                            <img src="img/doktor1.jpeg" alt="Dr. Marko Petrović" />
-                            <h3>Dr. Marko Petrović</h3>
-                            <p>Specijalist protetike s 20 godina iskustva. Stručnjak za estetsku stomatologiju.</p>
-                        </div>
-                        <div className="col-md-4 team-member">
-                            <img src="img/doktor2.jpeg" alt="Dr. Ana Kovačević" />
-                            <h3>Dr. Ana Kovačević</h3>
-                            <p>Specijalist oralne kirurgije, poznata po bezbolnim zahvatima i vrhunskom radu.</p>
-                        </div>
-                        <div className="col-md-4 team-member">
-                            <img src="img/doktor3.jpeg" alt="Dr. Ivan Jurić" />
-                            <h3>Dr. Ivan Jurić</h3>
-                            <p>Ekspert za ortodonciju, koji vraća osmijehe pacijentima svih uzrasta.</p>
-                        </div>
+                        {dentists.map(dentist => (
+                            <div className="col-md-4 team-member">
+                                <Img id={ dentist.featured_media} size="large" />
+                                
+                                <h3 dangerouslySetInnerHTML={{ __html: dentist.title.rendered }} />
+                                <p dangerouslySetInnerHTML={{ __html: dentist.content.rendered }}/>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
